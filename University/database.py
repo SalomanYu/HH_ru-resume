c72e5911baa271af7525ef042fda924be9566037
SalomanYu <rosya4806@gmail.com>
2022-07-08 19:29:19 +0300

    postupi parser

---
+++ University/database.py
@@ -0,0 +1,58 @@
+import sqlite3
+import os
+from typing import NamedTuple
+
+DATABASE = "shortnames_for_university"
+TABLE = "univers"
+
+class Connection(NamedTuple):
+    cursor: sqlite3.Cursor
+    db: sqlite3.Connection
+
+
+class University(NamedTuple):
+    fullname: str
+    shortname: str
+    city: str
+    url: str
+
+
+def connect_to_db() -> Connection:
+    os.makedirs(name='SQL', exist_ok=True)
+    db = sqlite3.connect(f'SQL/{DATABASE}.db')
+    cursor = db.cursor()
+    return Connection(cursor, db)
+
+
+# connect_to_db('hello')
+def create_table() -> None:
+    cursor, db = connect_to_db()
+
+    pattern = f"""
+        CREATE TABLE IF NOT EXISTS {TABLE}(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fullname VARCHAR(255),
+            shortname VARCHAR(255),
+            city VARCHAR(50),
+            url VARCHAR(255)
+        )
+    """
+    cursor.execute(pattern)
+    db.commit()
+    db.close()
+
+
+def add(data: University) -> None:
+    cursor, db = connect_to_db()
+    pattern = f"""
+        INSERT INTO {TABLE}(
+            fullname,
+            shortname,
+            city,
+            url
+        ) VALUES({','.join('?' for i in range(len(data)))})
+    """
+    cursor.execute(pattern, data)
+    db.commit()
+    db.close()
+    # logging.info("Added college - %s", data.name)
---
+++ University/selenium_getter_short_and_full_university_name.py
@@ -0,0 +1,39 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+import database
+from database import University
+
+from rich.console import Console
+from rich.progress import track
+
+option = Options()
+console = Console()
+option.headless = True
+web = webdriver.Chrome(options=option)
+
+def main():
+    page_num = 0
+    for _ in track(range(52), description="[yellow]Progress"):
+        page_num += 1
+        web.get(f'https://postupi.online/vuzi/?page_num={page_num}')
+        web.implicitly_wait(5)
+        sleep(1)
+        
+        univers =  [univer.get_attribute("href") for univer in web.find_elements(By.XPATH, "//h2[@class='list__h']//a")]
+        web.implicitly_wait(2)
+        for univer_url in univers:
+            web.get(univer_url)
+            web.implicitly_wait(3)
+            fullname = web.find_element(By.XPATH, "//h1[@class='bg-nd__h']").text
+            shortname = web.find_element(By.XPATH, "//div[@class='card-nd-pre-wrap']//h2").text
+            city = web.find_element(By.XPATH, "//p[@class='bg-nd__pre']").text.strip()
+
+            database.add(University(fullname=fullname, shortname=" ".join(shortname.split()[1:]),city=city, url=univer_url))
+            web.back()
+
+if __name__ == "__main__":
+    database.create_table()
+    main()
